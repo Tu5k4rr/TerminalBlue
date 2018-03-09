727a1cf83e93b9e2fe0b697cd4b6f5c9e5dd7f8b
Tu5k4rr <rory.vendee@gmail.com>
2018-03-09 20:21:02 +0000

    Rename SANS_miner_IP_xml-to-txt.ps1 to SANS_miner_IP.ps1

---
+++ data_lists/SANS_miner_IP.ps1
@@ -0,0 +1,22 @@
+﻿<#
+  Version:1.0
+  Author:@Tu5K4rr
+  Purpose:Convert XML to txt for SIEM IP list for miners
+  Usage: Help detect for traffic against known miners
+  Note: Thanks SANS for providing the XML data.
+  Test Platform Windows Server 2012 R2
+#>
+
+
+# Wget file
+wget https://isc.sans.edu/api/threatlist/miner -OutFile miner.xml
+#select xml for input
+$input_path = 'C:\Scripts\miner.xml'
+# Set regex query
+$regex = ‘\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b’
+#process file with regex and output to txt
+select-string -Path $input_path -Pattern $regex -AllMatches | % { $_.Matches } | % { $_.Value } > 'C:\Scripts\miner_ip.txt'
+Copy-Item "C:\Scripts\miner_ip.txt" -Destination C:\SIEM\LIST
+
+
+ 
