d7303a2a8b062b0ee37aab21c10fa81f95c003ac
Tu5k4rr <rory.vendee@gmail.com>
2019-09-03 13:40:50 +0100

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt clean
-sudo apt install build-essential htop python-dev cmake git python-pip -y
+sudo apt install -y build-essential htop python-dev cmake git python-pip 
